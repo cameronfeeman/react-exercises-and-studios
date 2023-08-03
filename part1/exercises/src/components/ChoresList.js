958032774a8b4e830d72d1e86720f498e07d983a
cameronfeeman <139170459+cameronfeeman@users.noreply.github.com>
2023-08-03 15:44:27 -0400

    Finished thru Add  More Movies Section

--- part1/exercises/src/components/ChoresList.js
+++ part1/exercises/src/components/ChoresList.js
@@ -1,3 +1,8 @@
+import classes from './ChoresList.module.css';
 export default function ChoresList () {
-   return <p>No content is here!</p>;
+   return <div> 
+      <h5 className={classes.choresHeading}></h5>
+         <ol></ol>
+      <p className={classes.choresText}> No content is here!</p>
+   </div>
 }
