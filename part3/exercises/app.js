7d78859b96e208aad184ee3ae20041a1affeda64
cameronfeeman <139170459+cameronfeeman@users.noreply.github.com>
2023-08-14 18:48:15 -0400

    update

---
+++ part3/exercises/app.js
@@ -0,0 +1,7 @@
+<div className="App">
+   <h1>My Hobby: Needlepoint</h1>
+   <div className="App">
+   <h1>My Hobby: Needlepoint</h1>
+   <HobbyIntroduction />
+</div>
+</div>
---
+++ part3/exercises/src/components/Introduction.js
@@ -0,0 +1,14 @@
+
+
+
+
+export default function HobbyIntroduction () {
+    return(
+        <div>
+            <h2>3 Fun Facts About this Hobby</h2>
+            <ul>Anyone can play at any age! </ul>
+            <ul>The longest Tennis Match was 11 hours and 5 minutes </ul>
+            <ul>THere are four main types of surfaces Tennis can be played on </ul>
+        </div>
+    );
+ }
